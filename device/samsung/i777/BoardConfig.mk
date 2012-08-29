b60d5f78e3a7656cc4489fc3f8fa300b633e86c7
task650 <task650@gmail.com>
2012-08-29 09:05:59 -0700

    Removed bullshit from BoardConfig.mk

--- device/samsung/i777/BoardConfig.mk
+++ device/samsung/i777/BoardConfig.mk
@@ -26,7 +26,9 @@ TARGET_KERNEL_SOURCE := kernel/samsung/smdk4210
 TARGET_KERNEL_CONFIG := cyanogenmod_i777_defconfig
 
 TARGET_SPECIFIC_HEADER_PATH += device/samsung/i777/overlay/include
-EXYNOS4210_ENHANCEMENTS := true
+
+# assert
+TARGET_OTA_ASSERT_DEVICE := i777,SGH-I777
 
 # Use the non-open-source parts, if they're present
 -include vendor/samsung/i777/BoardConfigVendor.mk
