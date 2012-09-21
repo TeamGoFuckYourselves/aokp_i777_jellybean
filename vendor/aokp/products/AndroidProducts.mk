168739caac20c6a2e3c93fdc0f7dd86bf32e64e9
task650 <task650@gmail.com>
2012-09-21 08:01:07 -0700

    Cleaned up Vendor bullshit.

--- vendor/aokp/products/AndroidProducts.mk
+++ vendor/aokp/products/AndroidProducts.mk
@@ -4,17 +4,23 @@ PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/crespo4g.mk \
     $(LOCAL_DIR)/d2tmo.mk \
     $(LOCAL_DIR)/d2att.mk \
+    $(LOCAL_DIR)/d2spr.mk \
     $(LOCAL_DIR)/i777.mk \
+    $(LOCAL_DIR)/d2vzw.mk \
     $(LOCAL_DIR)/galaxysmtd.mk \
     $(LOCAL_DIR)/fascinatemtd.mk \
     $(LOCAL_DIR)/grouper.mk \
     $(LOCAL_DIR)/hercules.mk \
     $(LOCAL_DIR)/maguro.mk \
     $(LOCAL_DIR)/n7000.mk \
-    $(LOCAL_DIR)/p5113.mk \
+    $(LOCAL_DIR)/p3100.mk \
+    $(LOCAL_DIR)/p3110.mk \
     $(LOCAL_DIR)/p3113.mk \
+    $(LOCAL_DIR)/p5100.mk \
+    $(LOCAL_DIR)/p5110.mk \
+    $(LOCAL_DIR)/p5113.mk \
     $(LOCAL_DIR)/stingray.mk \
     $(LOCAL_DIR)/toro.mk \
-    $(LOCAL_DIR)/torospr.mk \
+    $(LOCAL_DIR)/toroplus.mk \
     $(LOCAL_DIR)/vibrantmtd.mk \
     $(LOCAL_DIR)/wingray.mk
