6d6107c6c7217675670f527cbeba491a60bd9ea0
task650 <task650@gmail.com>
2012-09-21 14:38:12 -0700

    Just cleaning up our i777 repo

--- vendor/aokp/products/AndroidProducts.mk
+++ vendor/aokp/products/AndroidProducts.mk
@@ -5,9 +5,9 @@ PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/d2tmo.mk \
     $(LOCAL_DIR)/d2att.mk \
     $(LOCAL_DIR)/d2spr.mk \
-    $(LOCAL_DIR)/i777.mk \
     $(LOCAL_DIR)/d2vzw.mk \
     $(LOCAL_DIR)/galaxysmtd.mk \
+    $(LOCAL_DIR)/i777.mk \
     $(LOCAL_DIR)/fascinatemtd.mk \
     $(LOCAL_DIR)/grouper.mk \
     $(LOCAL_DIR)/hercules.mk \
@@ -19,8 +19,8 @@ PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/p5100.mk \
     $(LOCAL_DIR)/p5110.mk \
     $(LOCAL_DIR)/p5113.mk \
-    $(LOCAL_DIR)/stingray.mk \
     $(LOCAL_DIR)/toro.mk \
     $(LOCAL_DIR)/toroplus.mk \
     $(LOCAL_DIR)/vibrantmtd.mk \
-    $(LOCAL_DIR)/wingray.mk
+    $(LOCAL_DIR)/i9100.mk \
+    $(LOCAL_DIR)/i9100g.mk
--- vendor/aokp/vendorsetup.sh
+++ vendor/aokp/vendorsetup.sh
@@ -4,13 +4,12 @@ add_lunch_combo aokp_toroplus-userdebug
 add_lunch_combo aokp_grouper-userdebug
 add_lunch_combo aokp_crespo-userdebug
 add_lunch_combo aokp_crespo4g-userdebug
-add_lunch_combo aokp_captivatemtd-userdebug
 add_lunch_combo aokp_d2tmo-userdebug
 add_lunch_combo aokp_d2att-userdebug
 add_lunch_combo aokp_d2spr-userdebug
-add_lunch_combo aokp_i777-userdebug
 add_lunch_combo aokp_d2vzw-userdebug
 add_lunch_combo aokp_galaxysmtd-userdebug
+add_lunch_combo aokp_i777-userdebug
 add_lunch_combo aokp_hercules-userdebug
 add_lunch_combo aokp_n7000-userdebug
 add_lunch_combo aokp_p3100-userdebug
@@ -21,9 +20,7 @@ add_lunch_combo aokp_p5110-userdebug
 add_lunch_combo aokp_p5113-userdebug
 add_lunch_combo aokp_fascinatemtd-userdebug
 add_lunch_combo aokp_maguro-userdebug
-add_lunch_combo aokp_mezmerizemtd-userdebug
 add_lunch_combo aokp_grouper-userdebug
-add_lunch_combo aokp_showcasemtd-userdebug
-add_lunch_combo aokp_stingray-userdebug
 add_lunch_combo aokp_vibrantmtd-userdebug
-add_lunch_combo aokp_wingray-userdebug
+add_lunch_combo aokp_i9100-userdebug
+add_lunch_combo aokp_i9100g-userdebug
