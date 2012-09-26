7b159ac8d685e073a3561f5b5d329e92609c39c0
task650 <task650@gmail.com>
2012-09-26 07:17:47 -0700

    Alphabetize AndroidProducts.mk & vendorsetup.sh

--- vendor/aokp/products/AndroidProducts.mk
+++ vendor/aokp/products/AndroidProducts.mk
@@ -2,16 +2,18 @@ PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/captivatemtd.mk \
     $(LOCAL_DIR)/crespo.mk \
     $(LOCAL_DIR)/crespo4g.mk \
-    $(LOCAL_DIR)/d2tmo.mk \
     $(LOCAL_DIR)/d2att.mk \
     $(LOCAL_DIR)/d2spr.mk \
+    $(LOCAL_DIR)/d2tmo.mk \
     $(LOCAL_DIR)/d2vzw.mk \
-    $(LOCAL_DIR)/galaxysmtd.mk \
-    $(LOCAL_DIR)/i777.mk \
     $(LOCAL_DIR)/fascinatemtd.mk \
+    $(LOCAL_DIR)/galaxysmtd.mk \
     $(LOCAL_DIR)/grouper.mk \
     $(LOCAL_DIR)/hercules.mk \
+    $(LOCAL_DIR)/i777.mk \
     $(LOCAL_DIR)/i9300.mk \
+    $(LOCAL_DIR)/i9100.mk \
+    $(LOCAL_DIR)/i9100g.mk \
     $(LOCAL_DIR)/maguro.mk \
     $(LOCAL_DIR)/n7000.mk \
     $(LOCAL_DIR)/p3100.mk \
@@ -22,6 +24,4 @@ PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/p5113.mk \
     $(LOCAL_DIR)/toro.mk \
     $(LOCAL_DIR)/toroplus.mk \
-    $(LOCAL_DIR)/vibrantmtd.mk \
-    $(LOCAL_DIR)/i9100.mk \
-    $(LOCAL_DIR)/i9100g.mk
+    $(LOCAL_DIR)/vibrantmtd.mk
