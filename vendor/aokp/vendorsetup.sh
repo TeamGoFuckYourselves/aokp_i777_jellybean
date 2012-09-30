ec9a54f2463824cedec493a8cc864768c7165b31
task650 <task650@gmail.com>
2012-09-30 09:46:37 -0700

    More vendorsetup.sh organization.

--- vendor/aokp/vendorsetup.sh
+++ vendor/aokp/vendorsetup.sh
@@ -1,16 +1,18 @@
 add_lunch_combo aokp_maguro-userdebug
 add_lunch_combo aokp_toro-userdebug
+add_lunch_combo aokp_toroplus-userdebug
 add_lunch_combo aokp_crespo-userdebug
 add_lunch_combo aokp_crespo4g-userdebug
+add_lunch_combo aokp_grouper-userdebug
+add_lunch_combo aokp_captivatemtd-userdebug
+add_lunch_combo aokp_d2tmo-userdebug
 add_lunch_combo aokp_d2att-userdebug
 add_lunch_combo aokp_d2spr-userdebug
-add_lunch_combo aokp_d2tmo-userdebug
 add_lunch_combo aokp_d2usc-userdebug
 add_lunch_combo aokp_d2vzw-userdebug
 add_lunch_combo aokp_evita-userdebug
 add_lunch_combo aokp_fascinatemtd-userdebug
 add_lunch_combo aokp_galaxysmtd-userdebug
-add_lunch_combo aokp_grouper-userdebug
 add_lunch_combo aokp_hercules-userdebug
 add_lunch_combo aokp_i777-userdebug
 add_lunch_combo aokp_i9100-userdebug
@@ -24,5 +26,4 @@ add_lunch_combo aokp_p5100-userdebug
 add_lunch_combo aokp_p5110-userdebug
 add_lunch_combo aokp_p5113-userdebug
 add_lunch_combo aokp_tf101-userdebug
-add_lunch_combo aokp_toroplus-userdebug
 add_lunch_combo aokp_vibrantmtd-userdebug
